67ece44fc0b330460a9538c4d813e79d7f1ddfdd
buz <zhangqingsong21293@gmail.com>
2016-04-15 10:42:49 +0700

    up

---
+++ src/main/java/com/moom/face/MoomAccountApplication.java
@@ -0,0 +1,12 @@
+package com.moom.face;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoomAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoomAccountApplication.class, args);
+	}
+}
---
+++ src/test/java/com/moom/face/MoomAccountApplicationTests.java
@@ -0,0 +1,18 @@
+package com.moom.face;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MoomAccountApplication.class)
+@WebAppConfiguration
+public class MoomAccountApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
